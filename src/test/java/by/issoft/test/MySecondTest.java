25ac966eede06597fa107e1adf2971dbe58afc09
MarinaTsitova <statutam@gmail.com>
2020-11-02 11:18:07 +0300

    commit2

--- src/test/java/by/issoft/test/MySecondTest.java
+++ src/test/java/by/issoft/test/MySecondTest.java
@@ -2,6 +2,7 @@ package by.issoft.test;
 
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.ownText;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -10,11 +11,13 @@ public class MySecondTest {
     public void userCanLoginByUsername() {
         open("https://e-dostavka.by/");
 
-        $x("//body/div[@id='body']/div[1]/div[3]/div[2]/div[1]/nav[1]/ul[1]/li[9]/a[1]/i[1]").click();
+        $x("//body/div[@id='body']/div[1]/div[3]/div[2]/div[1]/nav[1]/ul[1]/li[10]/a[1]/i[1]").click();
         $x("//input[@id='searchtext']").setValue("Кофе молотый «Dallmayr» classic, 250 г.");
-        $x("//body/div[@id='body']/div[1]/div[3]/div[2]/div[1]/nav[1]/ul[1]/li[9]/a[1]/i[1]").click();
-        $x("//body/div[@id='body']/div[1]/div[3]/div[3]/div[1]/div[3]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]").click();
+        $x("//body/div[@id='body']/div[1]/div[3]/div[2]/div[1]/nav[1]/ul[1]/li[10]/a[1]/i[1]").click();
+        $x("//body/div[@id='body']/div[1]/div[3]/div[3]/div[1]/div[3]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/a[1]/img[1]").click();
 
+
+        //if($x("//span[contains(text(),'54.2')]").shouldHave(ownText("54.2")));
         String price = $x("//span[contains(text(),'54.2')]").getText();
 
 
